18de24f2f43ab20f7a7a399fe15c5508cb913423
jay-linearloop <jay.patel@linearloop.io>
2025-02-26 11:44:08 +0530

    added docker & Prometheus integration

--- src/jokes/jokes.service.ts
+++ src/jokes/jokes.service.ts
@@ -1,12 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpStatus } from '@nestjs/common';
+import { Counter, Histogram } from 'prom-client';
+import { InjectMetric } from '@willsoto/nestjs-prometheus';
 import axios from 'axios';
 
 @Injectable()
 export class JokesService {
+  constructor(
+    @InjectMetric('jokes_requests_total')
+    private readonly jokesMetric: Counter<string>,
+    @InjectMetric('jokes_http_requests')
+    private readonly apiMetrics: Histogram<string>,
+  ) {}
+
   async getJoke() {
-    const response = await axios.get(
-      'https://official-joke-api.appspot.com/random_joke',
-    );
-    return response.data;
+    const startTime = Date.now();
+    try {
+      this.jokesMetric.inc();
+      const response = await axios.get(
+        'https://official-joke-api.appspot.com/random_joke',
+      );
+      
+      const duration = (Date.now() - startTime) / 1000; // Convert to seconds
+      this.apiMetrics.observe(
+        { method: 'GET', status_code: response.status, path: '/jokes' },
+        duration,
+      );
+      
+      return response.data;
+    } catch (error) {
+      const duration = (Date.now() - startTime) / 1000;
+      this.apiMetrics.observe(
+        { 
+          method: 'GET', 
+          status_code: error.response?.status || 500, 
+          path: '/jokes' 
+        },
+        duration,
+      );
+      throw error;
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Counter } from 'prom-client';
+import { InjectMetric } from '@willsoto/nestjs-prometheus';
 import axios from 'axios';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectMetric('users_requests_total')
+    private readonly usersMetric: Counter<string>,
+  ) {}
+
   async getUsers() {
+    this.usersMetric.inc();
     const response = await axios.get('https://randomuser.me/api/?results=6');
     return response.data.results;
   }
